e3080ab67071fb25c3016d2a714d43dc4b449eff
strommj20@gmail.com <strommj@uwec.edu>
2018-09-17 21:35:20 -0500

    New Functionality: User must login before visiting site

--- index.js
+++ index.js
@@ -1,48 +1,70 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString:process.env.DATABASE_URL,
   ssl: true
 });
 
+// Unauthorized users are redirected to the login screen
+var authenticate = function(req, res, next) {
+  if (req.session && req.session.loggedIn)
+    return next();
+  else
+    return res.redirect('/login');
+};
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(session({
+    secret: 'May the force be with you.',
+    resave: true,
+    saveUninitialized: false
+  }))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+
+  // Login and Authentication
   .get('/login', (req, res) => res.render('pages/login'))
   .get('/login/authentication', async (req, res) => {
-    var username = req.query.username;
-    var password = req.query.password;
+    req.session.user = req.query.username;
 
     try {
       const client = await pool.connect();
-      const result = await client.query("SELECT password FROM users");
+      const result = await client.query("SELECT password FROM users WHERE login = $1", [req.session.user]);
       const results = { 'results': (result) ? result.rows : null};
       client.release();
 
-        console.log("res: " + result.rows[0].password);
-        
-      /*  if (err) throw err;
+      if (result.rows[0].password == req.query.password) {
+        console.log("Login successful!")
+        req.session.loggedIn = true;
+        res.redirect('/');
 
-        if (res != null) {
-          return res;
-        }
-      });*/
-
-      if (result.rows[0].password == password) {
-        res.render('pages/index');
       } else {
         console.log("Sorry, incorrect password");
       }
     } catch (err) {
       console.error(err);
-      res.send("error " + err);
+      res.send("error: " + err);
     }
   })
-  .get('/db', async (req, res) => {
+
+  // Home page
+  .get('/', authenticate, async (req, res) => {
+    /*try {
+
+    } catch (err) {
+      console.error(err);
+      res.send("error: " + err);
+    }*/
+
+    res.render('pages/home');
+  })
+
+  // Test database request
+  .get('/db', authenticate, async (req, res) => {
     try {
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM applications');
@@ -54,4 +76,4 @@ express()
       res.send("Error " + err);
     }
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
